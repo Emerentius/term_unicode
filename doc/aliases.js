da01dfa9355ea98afa694cd1302457452be5afb1
Emerentius <emerentius@arcor.de>
2018-08-27 04:16:02 +0200

    initial commit

---
+++ doc/aliases.js
@@ -0,0 +1,2 @@
+var ALIASES = {};
+ALIASES['term_unicode'] = {};
---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var N = null;var searchIndex = {};
+searchIndex["term_unicode"]={"doc":"","items":[[5,"locale_requests_utf8","term_unicode","Try to read the preferred encoding from the locale.",N,[[],["bool"]]],[5,"supports_single_width_chars","","Tests support for single width unicode characters by printing a 2 byte, 1 wide character and comparing cursor positions before and after.",N,[[],["result",["bool"]]]],[5,"supports_double_width_chars","","Tests support for double width unicode characters by printing a 3 byte, 2 wide character and comparing cursor positions before and after.  This is required to correctly display Chinese, Japanese and Korean (CJK) scripts.",N,[[],["result",["bool"]]]]],"paths":[]};
+initSearch(searchIndex);
